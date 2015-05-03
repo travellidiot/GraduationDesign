a2e27958fe7da5831a4b55b9411e68b07d55b90a
travellidiot <travellidiot@gmail.com>
2015-05-03 23:54:29 +0800

    直方图
    直方图接口实现，HSV颜色空间的Hue直方图实现。

--- FeatureExtracter/FeatureExtracter/FeatureExtracter.cs
+++ FeatureExtracter/FeatureExtracter/FeatureExtracter.cs
@@ -14,7 +14,8 @@ namespace FeatureExtracter
     public class FeatureExtracter
     {
         private CoordinateMapper coordinateMapper = null;
-        private ColorSpacePoint[] colorMappedToDepthPoints = null;
+        private ColorSpacePoint[] depthMappedToColorPoints = null;
+        private CameraSpacePoint[][] jointsInCameraSpace = null;
         private byte[] depthBytes = null;
         private byte[] bodyIndexBytes = null;
         private BodyData[] bodies = null;
@@ -52,15 +53,93 @@ namespace FeatureExtracter
             {
                 this.bodies = br.ReadAllBodies();
             }
+
+            jointsInCameraSpace = new CameraSpacePoint[bodies.Length][];
+            for (int i = 0; i < this.bodies.Length; i++)
+            {
+                BodyData body = this.bodies[i];
+                if (body.TrackingId == 0)
+                    continue;
+
+                jointsInCameraSpace[i] = new CameraSpacePoint[body.Joints.Count];
+                foreach (Joint joint in body.Joints.Values)
+                {
+                    jointsInCameraSpace[i][(int)joint.JointType] = joint.Position;
+                }
+            }
+
+            depthMappedToColorPoints = new ColorSpacePoint[depthBytes.Length];
+            unsafe
+            {
+                fixed (byte* scan0 = &depthBytes[0])
+                {
+                    IntPtr ptr = (IntPtr)scan0;
+                    coordinateMapper.MapDepthFrameToColorSpaceUsingIntPtr(ptr,
+                        (uint)depthBytes.Length, depthMappedToColorPoints);
+                }
+            }
         }
 
         public void test()
         {
+            
+        }
+
+        private DepthSpacePoint[] getJointsPosInColorSpace(int bodyIndex)
+        {
+            DepthSpacePoint[] jointsInDepthSpacePoints =new DepthSpacePoint[bodies[bodyIndex].Joints.Count];    
+            coordinateMapper.MapCameraPointsToDepthSpace(jointsInCameraSpace[bodyIndex], jointsInDepthSpacePoints);
+
+            return jointsInDepthSpacePoints;
+        }
+
+        /// <summary>
+        /// 返回包围盒，Tuple值类型为<左边界，上边界，右边界，下边界>
+        /// </summary>
+        /// <param name="joints">需要做包围盒的节点数组</param>
+        /// <returns></returns>
+        private Tuple<float, float, float, float> getBox(DepthSpacePoint[] joints)
+        {
+            IEnumerable<float> coorX = Enumerable.Select<DepthSpacePoint, float>(joints, (j) => j.X);
+            IEnumerable<float> coorY = Enumerable.Select<DepthSpacePoint, float>(joints, (j) => j.Y);
+
+            var rect = Tuple.Create<float, float, float, float>(coorX.Min(), coorY.Max(), coorX.Max(), coorY.Min());
+
+            return rect;
+        }
+        private Tuple<float, float, float, float> getUpBodyBox(DepthSpacePoint[] joints)
+        {
+            DepthSpacePoint[] upBodyJoints = new DepthSpacePoint[]
+            {
+                joints[(int)JointType.Neck], 
+                joints[(int)JointType.HandLeft], 
+                joints[(int)JointType.HandRight],
+                joints[(int)JointType.ShoulderLeft],
+                joints[(int)JointType.ShoulderRight],
+                joints[(int)JointType.SpineBase]
+            };
+
+            return getBox(upBodyJoints);
+        }
+
+        public IHistogram<byte> UpBodyHueHisto(int bodyIndex)
+        {
+            DepthSpacePoint[] jointsInDepthSpacePoints = getJointsPosInColorSpace(bodyIndex);
+            Tuple<float, float, float, float> rect = getUpBodyBox(jointsInDepthSpacePoints);
+            
+            
+            HueHisto hhisto = new HueHisto();
             unsafe
             {
-                fixed (byte* p = (&this.depthBytes[0]))
+                fixed (byte* scan0 = &bodyIndexBytes[0])
                 {
-                    
+                    for (float i = rect.Item4; i < rect.Item2; i++)
+                    {
+                        for (float j = rect.Item1; j < rect.Item3; j++)
+                        {
+                            
+                        }
+                    }
                 }
             }
         }
--- FeatureExtracter/FeatureExtracter/Program.cs
+++ FeatureExtracter/FeatureExtracter/Program.cs
@@ -15,6 +15,13 @@ namespace FeatureExtracter
         
         static void Main(string[] args)
         {
+            string colorFile = @"C:\Users\koala\Documents\GitHub\GraduationDesign\Data\ColorImage.png";
+            string depthFile = @"C:\Users\koala\Documents\GitHub\GraduationDesign\Data\DepthData.dp";
+            string bodyIndexFile = @"C:\Users\koala\Documents\GitHub\GraduationDesign\Data\BodyIndex.bi";
+
+            FeatureExtracter extracter = new FeatureExtracter();
+            extracter.LoadFiles(colorFile, depthFile, bodyIndexFile, @"");
+            extracter.test();
         }
     }
 }
